4cdf8a174bb6533c7a46bada74cd18e8c0aec705
BKinger4 <bruin.kunnas@ucalgary.ca>
2019-02-28 10:47:28 -0700

    Updated Files for Log In

--- login.js
+++ login.js
@@ -1,30 +1,21 @@
 //Login Account
-var email = document.getElementById('email').value
-var password = document.getElementById('password').value
-var data = 'accounts.json';
-var users = JSON.parse(data);
 
-for(i = 0; i < users.length; i++) {
-  if(email == users.accounts[i].email && password == users.accounts[i].password) {
-    console.log(json.accounts[i].email);
-    console.log(json.accounts[i].password);
-    alert("Successful Login");
+
+function logIn() {
+  var email = document.getElementById('email').value;
+  var password = document.getElementById('password').value;
+  var success = 0;
+
+  for(i = 0; i < users.accounts.length; i++) {
+    if(email == users.accounts[i].email && password == users.accounts[i].password) {
+      success += 1;
+      alert("Successful Login!");
+      window.location.href = "search.html";
+      break;
+    }
   }
-  else {
+
+  if(success == 0) {
     alert("Incorrect email or password! Please try again.");
   }
-}
-
-/*
-function logIn(data) {
-
-  for(i = 0; i < data.length; i++) {
-    if (email == data.accounts[i].email && password == data.accounts[i].password) {
-      alert(email + " successfully logged in!");
-    }
-    else {
-      alert("Incorrect username or password!");
-    }
-  }
 };
-*/
