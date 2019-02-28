17233f270245dd8ea2a71192217b422fa767da0f
BKinger4 <bruin.kunnas@ucalgary.ca>
2019-02-27 22:55:30 -0700

    Updated Log in page and application form page

--- login.js
+++ login.js
@@ -1,19 +1,30 @@
 //Login Account
-function login(form) {
-  var data = JSON.parse(accounts);
-  var location = '';
-  var username = document.getElementById("username").value
-  var password = document.getElementById("password").value
+var email = document.getElementById('email').value
+var password = document.getElementById('password').value
+var data = 'accounts.json';
+var users = JSON.parse(data);
 
-  for(i=0; i<JSON.parse(accounts).length; i++) {
-    if (username == data[i].email && password == data[i].password) {
-      location = "page1.html"; //Change name to whatever the file is named for the homepage
-      console.log(username + " successfully logged in!");
+for(i = 0; i < users.length; i++) {
+  if(email == users.accounts[i].email && password == users.accounts[i].password) {
+    console.log(json.accounts[i].email);
+    console.log(json.accounts[i].password);
+    alert("Successful Login");
+  }
+  else {
+    alert("Incorrect email or password! Please try again.");
+  }
+}
+
+/*
+function logIn(data) {
+
+  for(i = 0; i < data.length; i++) {
+    if (email == data.accounts[i].email && password == data.accounts[i].password) {
+      alert(email + " successfully logged in!");
     }
     else {
       alert("Incorrect username or password!");
     }
   }
-}
-
-login();
+};
+*/
