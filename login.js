e66ff4aa7f1d711bce8d2fa739b9af6905ba48b4
BKinger4 <bruin.kunnas@ucalgary.ca>
2019-03-12 10:20:16 -0600

    update

--- login.js
+++ login.js
@@ -5,20 +5,22 @@ function logIn() {
   //Takes information from index.html as input for email and password.
   var email = document.getElementById('email').value;
   var password = document.getElementById('password').value;
-  var success = 0;
+  var success = false;
 
   //Iterates through JSON file to check if the information is an accepted account.
   for(i = 0; i < users.accounts.length; i++) {
     if(email == users.accounts[i].email && password == users.accounts[i].password) {
-      success += 1;
-      alert("Successful Login!");
+      success = true;
+      // alert("Successful Login!");
       window.location.href = "search.html";
       break;
     }
   }
 
   //Shows alert if the email or password is incorrect.
-  if(success == 0) {
-    alert("Incorrect email or password! Please try again.");
+  if(success == false) {
+        var x = document.getElementById("wrongInfo").style.opacity = '1';
+        //x.style.opacity = '1';
+    // alert("Incorrect email or password! Please try again.");
   }
 };
