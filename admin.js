45072267708d117cb87adcc56462000cc177756e
BKinger4 <bruin.kunnas@ucalgary.ca>
2019-03-12 10:19:39 -0600

    admin controls

---
+++ admin.js
@@ -0,0 +1,27 @@
+//SENG 300 L01 - Group 10 (Robbie K., Sam L., Sandesh R., Jacob H., Bruin K.)
+//Administration Controls
+
+//Allows admin to approve user applications.
+function approveUsers() {
+
+}
+
+//Allows admin to remove users based on certain criteria.
+function removeUser() {
+
+}
+
+//Remove books from library and add them to damaged book list.
+function addDamagedBooks() {
+  
+}
+
+//Allows admin to view history of orders.
+function viewHistory() {
+
+}
+
+//Allows admin to order new products for the library.
+function orderProducts() {
+
+}
