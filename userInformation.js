79345d8430581deb422e661c534dd27d5f3117f4
SamWOW99 <samlaurie@ymail.com>
2019-03-14 15:52:42 -0600

    User Info pulled from database for hardcoded user.

--- userInformation.js
+++ userInformation.js
@@ -1,66 +1,42 @@
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyD4WbKGk37cw68SM4zAA9ZakywahHyeyGI",
-  authDomain: "librarymanagmentsystem-ce706.firebaseapp.com",
-  databaseURL: "https://librarymanagmentsystem-ce706.firebaseio.com",
-  projectId: "librarymanagmentsystem-ce706",
-  storageBucket: "librarymanagmentsystem-ce706.appspot.com",
-  messagingSenderId: "533299745296"
-};
-firebase.initializeApp(config);
-
-var database = firebase.firestore();
-
-
 
 //Get User Information
-function userFees() {
-  var fees = database.collection("users").doc("Sam Laurie").collection("History").doc("S6g25Ra2kHJQKI1gYTLi");
-  fees.get().then(function(doc) {
-    console.log(doc.get("feesOwed"));
+function userInfo() {
+  var info = database.collection("/users/Sam Laurie/History").doc("S6g25Ra2kHJQKI1gYTLi");
+  info.get().then(function(doc) {
     var feesOwed = doc.get("feesOwed");
     var feesPaid = doc.get("feesPaid");
     var feesTotal = doc.get("feesTotal");
+    var book1Name = doc.get("book1Name");
+    var dateOut1 = doc.get("dateOut1");
+    var dateRet1 = doc.get("dateRet1");
+    var book2Name = doc.get("book2Name");
+    var dateOut2 = doc.get("dateOut2");
+    var dateRet2 = doc.get("dateRet2");
+    var book3Name = doc.get("book3Name");
+    var dateOut3 = doc.get("dateOut3");
+    var dateRet3 = doc.get("dateRet3");
+    var book4Name = doc.get("book4Name");
+    var dateOut4 = doc.get("dateOut4");
+    var dateRet4 = doc.get("dateRet4");
+    var book5Name = doc.get("book5Name");
+    var dateOut5 = doc.get("dateOut5");
+    var dateRet5 = doc.get("dateRet5");
 
-    var table = document.getElementById("t01");
-    var feesTable = table.insertRow(1);
-    var feesTable2 feesTable.insertCell(1);
-    feesTable2.innerHTML = feesOwed;
-
-    window.alert("Refreshed");
+    document.getElementById("feesOwed").innerHTML = feesOwed;
+    document.getElementById("book1Name").innerHTML = book1Name;
+    document.getElementById("book2Name").innerHTML = book2Name;
+    document.getElementById("book3Name").innerHTML = book3Name;
+    document.getElementById("book4Name").innerHTML = book4Name;
+    document.getElementById("book5Name").innerHTML = book5Name;
+    document.getElementById("dateOut1").innerHTML = dateOut1;
+    document.getElementById("dateOut2").innerHTML = dateOut2;
+    document.getElementById("dateOut3").innerHTML = dateOut3;
+    document.getElementById("dateOut4").innerHTML = dateOut4;
+    document.getElementById("dateOut5").innerHTML = dateOut5;
+    document.getElementById("dateRet1").innerHTML = dateRet1;
+    document.getElementById("dateRet2").innerHTML = dateRet2;
+    document.getElementById("dateRet3").innerHTML = dateRet3;
+    document.getElementById("dateRet4").innerHTML = dateRet4;
+    document.getElementById("dateRet5").innerHTML = dateRet4;
   });
-
-  //Get user fees
-  //Get user books borrowed
-  //Get user return dates
-}
-
-function search(){
-  var book = database.collection("books").doc();
-  book.get().then(function(doc) {
-    console.log(doc.get("book_name"));
-    var title = doc.get("book_name");
-    var author = doc.get("book_author");
-    var published = doc.get("Published");
-    var quantity = doc.get("Quantity");
-    var id = doc.get("book_id");
-
-    var table = document.getElementById("bookTable");
-    var row = table.insertRow(1);
-    var cell0 = row.insertCell(0);
-    var cell1 = row.insertCell(1);
-    var cell2 = row.insertCell(2);
-    var cell3 = row.insertCell(3);
-    var cell4 = row.insertCell(4);
-    var cell5 = row.insertCell(5);
-    cell0.innerHTML = title;
-    cell1.innerHTML = author;
-    cell2.innerHTML = published;
-    cell3.innerHTML = id;
-    cell4.innerHTML = quantity;
-    cell5.innerHTML = "nosoftcopy";
-
-
-  });
-
 }
