0f90179fd2e7a228699050df021eacd6e93475e6
BKinger4 <bruin.kunnas@ucalgary.ca>
2019-03-14 14:39:21 -0600

    update

--- admin.js
+++ admin.js
@@ -35,10 +35,29 @@ function removeUser() {
 
 //Remove books from library and add them to damaged book list.
 function addDamagedBooks() {
-  var damagedBook = document.getElementById('book').value;
+  var name = document.getElementById('book').value;
 
   if (database.collection("books").doc(damagedBook) != null) {
+    var book = database.collection("books").doc(name);
 
+    book.get().then(function(book) {
+      var bookName = book.get("book_name");
+      var published = book.get("published");
+      var quantity = book.get("quantity").value;
+      var author = book.get("book_author");
+      var id = book.get("book_id");
+
+      database.collection("damagedBooks").doc(bookName).set({
+        book_name: bookName,
+        published: published,
+        book_author: author,
+        book_id: id,
+      });
+    }
+    quantity -= 1;
+    database.collection("books").doc(book).update({
+      quantity: quantity
+    });
   }
 }
 
@@ -47,10 +66,11 @@ function viewHistory() {
   var book = document.getElementById('book').value;
   var currentBook = database.collection("books").doc("History").collection("history");
 
-  // currentBook.get().then(function(doc) {
-  //   var title = doc.get("book_name");
-  //   var
-  // });
+  currentBook.get().then(function(doc) {
+    var title = doc.get("book_name");
+    var history = doc.collection("History");
+
+  });
 }
 
 //Allows admin to order new products for the library.
