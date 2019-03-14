40ae47e62ad30851d53ba4c6a0a07407ba75ce91
JacobHuber <xjacobhuber@gmail.com>
2019-03-14 11:19:49 -0600

    It looks nicer!

--- functions.js
+++ functions.js
@@ -2,11 +2,13 @@ function search(){
   var searchEntry = document.getElementById('search').value;
   var book = database.collection("books").doc("The Name of The Wind");
   book.get().then(function(doc) {
-    console.log(doc.get("book_name"));
     var title = doc.get("book_name");
-    var table = document.getElementById("bookTable");
+    var table = document.getElementById("t01");
     var row = table.insertRow(1);
     var cell1 = row.insertCell(0);
+    for (let i = 1; i < table.rows[0].cells.length; i++) {
+      row.insertCell(i);
+    }
     cell1.innerHTML = title;
   });
 
