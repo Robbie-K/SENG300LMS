294a30a45a2a6c94371e8ccfe7987d26d9cae287
RegmiS <sandeshreg35@gmail.com>
2019-03-20 14:46:58 -0600

    replacing old files with new ones

--- README.md
+++ README.md
@@ -1,19 +1,27 @@
-# SENG300LMS
-# Jacob Huber, Robert Karg, Bruin Kunnas, Sam Laurie, Sandesh Regmi
-
+# SENG300LMS L01 - Group 10
 Private repo for the SENG300 Library Management System
 
 Step 1: Click on this link to go to the log in page.
 https://regmis.github.io/SENG300LMS/
 
-Step 2: Create a login account. This account is not automatically added to
-        the system currently.
+Step 2: Create a login account. 
 
 Step 3: Log in using this account:
-        email: john.doe@utoronto.ca
-        password: L33t5p3@k
+        email: bruinkunnas@ucalgary.ca
+        password: 4
 
-Step 4: Buttons and search bar are not implemented as we are working on the
-        data base.
+Step 4: Press the search button and a list of books that we
+        currently have in the database will be listed.
+        
+Step 5: Click on account at the top to view the outline of
+        what each user will see for the accounts.
+        
+Step 6: Click refresh at the bottom to update some information.
+        
+Step 7: Click home if you wish to return to the search menu.
 
-Step 5: Log out.
+Step 8: Click Log out.
+
+Step 9: Click on contact us.
+
+Step 10: Write us something that we need to change or fix.
--- login.js
+++ login.js
@@ -1,24 +1,34 @@
 //Login Account
 
+// Called on form submission from index.html
 
 function logIn() {
-  //Takes information from index.html as input for email and password.
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
-  var success = 0;
+	//Takes information from index.html as input for email and password.
+	let email = document.getElementById('email');
+	let password = document.getElementById('password');
 
-  //Iterates through JSON file to check if the information is an accepted account.
-  for(i = 0; i < users.accounts.length; i++) {
-    if(email == users.accounts[i].email && password == users.accounts[i].password) {
-      success += 1;
-      alert("Successful Login!");
-      window.location.href = "search.html";
-      break;
-    }
-  }
 
-  //Shows alert if the email or password is incorrect.
-  if(success == 0) {
-    alert("Incorrect email or password! Please try again.");
-  }
+	let success;
+
+	let userQuery = database.collection("users").where("email", "==", email.value);
+
+	userQuery.get().then(function(snapshot) {
+		if (!snapshot.empty) {
+			let user = snapshot.docs[0];
+
+			let fetchedPassword = user.get("password");
+			if (fetchedPassword == password.value) {
+				window.location.href = "search.html";
+			} else {
+				let errorSpan = document.getElementById("error");
+				errorSpan.innerHTML = "Email or password are invalid.";
+
+				email.value = "";
+				password.value = "";
+			}
+
+		}
+	});
+
+
 };
