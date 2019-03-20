bbfd56774fa718d2a0f994265dc28a9534822cb0
BKinger4 <bruin.kunnas@ucalgary.ca>
2019-03-20 16:02:44 -0600

    Update files

--- login.js
+++ login.js
@@ -1,26 +1,34 @@
 //Login Account
 
+// Called on form submission from index.html
 
 function logIn() {
-  //Takes information from index.html as input for email and password.
-  var email = document.getElementById('email').value;
-  var password = document.getElementById('password').value;
-  var success = false;
+	//Takes information from index.html as input for email and password.
+	let email = document.getElementById('email');
+	let password = document.getElementById('password');
 
-  //Iterates through JSON file to check if the information is an accepted account.
-  for(i = 0; i < users.accounts.length; i++) {
-    if(email == users.accounts[i].email && password == users.accounts[i].password) {
-      success = true;
-      // alert("Successful Login!");
-      window.location.href = "search.html";
-      break;
-    }
-  }
 
-  //Shows alert if the email or password is incorrect.
-  if(success == false) {
-        var x = document.getElementById("wrongInfo").style.opacity = '1';
-        //x.style.opacity = '1';
-    // alert("Incorrect email or password! Please try again.");
-  }
+	let success;
+
+	let userQuery = database.collection("users").where("email", "==", email.value);
+
+	userQuery.get().then(function(snapshot) {
+		if (!snapshot.empty) {
+			let user = snapshot.docs[0];
+
+			let fetchedPassword = user.get("password");
+			if (fetchedPassword == password.value) {
+				window.location.href = "search.html";
+			} else {
+				let errorSpan = document.getElementById("error");
+				errorSpan.innerHTML = "Email or password are invalid.";
+
+				email.value = "";
+				password.value = "";
+			}
+
+		}
+	});
+
+
 };
