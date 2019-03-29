03ac8ea1222c29c8e57be4fe0299d0259272cf8f
RegmiS <sandeshreg35@gmail.com>
2019-03-29 13:08:40 -0600

    commenting files in functions.js

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-// Initialize Firebase
+// Initialize Firebase server api
 var config = {
   apiKey: "AIzaSyD4WbKGk37cw68SM4zAA9ZakywahHyeyGI",
   authDomain: "librarymanagmentsystem-ce706.firebaseapp.com",
@@ -9,24 +9,33 @@ var config = {
 };
 firebase.initializeApp(config);
 
+// setting the filter and database to be global variables to be used
 var database = firebase.firestore();
 var filter = "";
 
+// function that searches for books/authors/etc
 function search(){
+  // search entry gets what user typed and also the database of books
   var searchEntry = document.getElementById('search').value;
   var book = database.collection("books");
   var table = document.getElementById("bookTable");
 
+  // makes sure to remove all the rows before the code starts
+  // this allows for table to look normaly every search
   removeRows();
+  // counts rows up and gets books
   var rowCount = 1;
   book.get().then(function(querySnapshot) {
     querySnapshot.forEach(function (documentSnapshot){
+      // going through each book in the database to allow for searching filters
+      // getting all the info related to all books
       var data = documentSnapshot.data();
       var bookname = data.book_name;
       var author = data.book_author;
       var genre = data.Genre;
       var searchFilter = "";
 
+      // checking if the filter they chose matches the filter in search
       if(filter == "author"){
         var searchFilter = author.toLowerCase();
       }
@@ -37,9 +46,11 @@ function search(){
         var searchFilter = bookname.toLowerCase();
       }
 
+      // setting the filter and search value to lower case
       var searchEntryLower = searchEntry.toLowerCase();
 
       if(searchFilter.includes(searchEntryLower) == true){
+        // adding a new row in the table if what they wanted is there.
         var published = data.Published;
         var quantity = data.Quantity;
         var id = data.book_id;
@@ -63,6 +74,7 @@ function search(){
   });
 }
 
+// goes through and removes all the rows of the table except for the first one
 function removeRows(){
   var table = document.getElementById("bookTable");
   var rowlength = table.rows.length;
@@ -72,6 +84,7 @@ function removeRows(){
   }
 }
 
+// makes it so that the search bar activitates when they hit "enter"
 var input = document.getElementById("search");
 input.addEventListener("keyup", function(event) {
   if (event.keyCode === 13) {
@@ -80,22 +93,26 @@ input.addEventListener("keyup", function(event) {
   }
 });
 
+// setting the filter to search books by their title
 function setFilterTitle(){
   filter = "title";
   var element = document.getElementById("currentFilter");
   document.getElementById("filterValue").innerHTML = "Search: Title";
 }
 
+// setting the filter to search books by their author
 function setFilterAuthor(){
   filter = "author";
   document.getElementById("filterValue").innerHTML = "Search: Author";
 }
 
+// setting filter to search books by their genre
 function setFilterGenre(){
   filter = "genre";
   document.getElementById("filterValue").innerHTML = "Search: Genre";
 }
 
+// check status page for the users
 function checkStatus()
 {
   var user = database.collection('users');
