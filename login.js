caa8a766fa7e08a506c03ef6c190faaafa641a7f
SamWOW99 <samlaurie@ymail.com>
2019-03-29 23:02:59 -0600

    Merged with master

--- login.js
+++ login.js
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-//Login Account
-
-// Called on form submission from index.html
-
-function logIn() {
-	//Takes information from index.html as input for email and password.
-	let email = document.getElementById('email');
-	let password = document.getElementById('password');
-
-
-	let success;
-
-	let userQuery = database.collection("users").where("email", "==", email.value);
-
-	userQuery.get().then(function(snapshot) {
-		if (!snapshot.empty) {
-			let user = snapshot.docs[0];
-
-			let fetchedPassword = user.get("password");
-			if (fetchedPassword == password.value) {
-				window.location.href = "search.html";
-			} else {
-				let errorSpan = document.getElementById("error");
-				errorSpan.innerHTML = "Email or password are invalid.";
-
-				email.value = "";
-				password.value = "";
-			}
-
-		}
-	});
-
-
-};
-=======
 //Login Account
 
 // Called on form submission from index.html
@@ -70,4 +34,3 @@ function logIn() {
 
 
 };
->>>>>>> master
