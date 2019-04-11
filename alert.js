e90029125b2f278e0ca8382f739842bef7845bb1
JacobHuber <xjacobhuber@gmail.com>
2019-04-11 09:21:43 -0600

    Comments!

--- alert.js
+++ alert.js
@@ -1,14 +1,18 @@
+// Makes an alert box pop up with the given message
+// and evaluates the given code
 function make_alert(message, toCall) {
 	let box = document.getElementById("alert-outer");
-	box.style.display = "initial";
+	box.style.display = "initial"; // Make box visible (default display = none)
 
 	let text = document.getElementById("alert-message");
-	text.innerHTML = message;
+	text.innerHTML = message; // Change message
 
+	// Make a button that closes the box and runs the given
+	// code
 	let button = document.getElementById("alert-button");
 	button.onclick = function() {
 		let alert_box = this.parentNode.parentNode;
-		alert_box.style.display = 'none';
-		eval(toCall);
+		alert_box.style.display = 'none'; // Hide box
+		eval(toCall); // Run code
 	}
 }
--- genreButton.js
+++ genreButton.js
@@ -1,10 +1,13 @@
+
+// Makes genre buttons switch between
+// each other when clicking on one
 let buttons = document.getElementsByClassName("genreButton");
 
 for (let i = 0; i < buttons.length; i++) {
 	buttons[i].addEventListener('click', function() {
 		let remove = document.getElementsByClassName('genreButton active');
-		remove[0].className = 'genreButton';
+		remove[0].className = 'genreButton'; // 'Unselects' button
 
-		this.className = 'genreButton active';
+		this.className = 'genreButton active'; // 'Selects' button
 	});
 }
